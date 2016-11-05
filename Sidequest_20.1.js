74e659638e8b885def6e13f9158d1058098e4c81
mhq199657 <mhq199657@163.com>
2016-11-06 00:20:08 +0800

    Add files via upload

--- Sidequest_20.1.js
+++ Sidequest_20.1.js
@@ -1,3 +1,4 @@
+//Task 1
 function stream_constant(n){
 	return pair(n,function(){return stream_constant(n);});
 }
@@ -18,6 +19,12 @@ function regenerate_truncated_whisper(tw) {
 		return fold_stream(regenerate_truncated_whisper(differences(tw)),head(tw));
 	}
 }
+ display(eval_stream(regenerate_truncated_whisper(list(9, 6, -5, -18, -27)), 9));
+// [9, [6, [-5, [-18, [-27, [-26, [-9, [30, [97, []]]]]]]]]]
+
+// Task 2
+
+
 
 	function split_stream(s1,s2){//Total stream length 1 or list(val1,val2,...)
 		if(is_empty_list(s2)||!equal(head(s2),false)){
@@ -64,12 +71,12 @@ function regenerate_truncated_whisper(tw) {
 	    var h = is_empty_list(known)?head(unknown):head(known);
 	    if(is_empty_list(unknown)){
 	        var t = regenerate_truncated_whisper(stream_to_list(known));
-	        count =count +1;
 	        return pair(h,function(){return stream_tail(t);});
 	    }else{
 	        var s=split(unknown);
-	        var r = head(tail(split(unknown)));
+	        var r = !is_empty_list(tail(s))?head(tail(s)):[];
 	        if(is_empty_list(head(s))||is_empty_list(known)){
+                var r1= pair(h, function(){return stream_tail(regenerate(stream_append(known,head(s)),r));});
 
 	            return pair(h, function(){return stream_tail(regenerate(stream_append(known,head(s)),r));});
 	        }else{
@@ -78,12 +85,16 @@ function regenerate_truncated_whisper(tw) {
 	    }
 
 	}
+	function regenerate_corrupted_whisper(s){
+	    return regenerate([],s);
+	}
 
-var count = 0;
 var corrupted_whisper = list_to_stream(list(4, false, 6, false, false, false,
  -2, false, -2.5, false, false));
- eval_stream(regenerate([],corrupted_whisper),1);
+ eval_stream(regenerate([],corrupted_whisper),20);
 
+
+// Task 3
 function to_fun(s){
 	function dispatch(x){
 		if(x<=0){
@@ -98,7 +109,7 @@ function to_fun(s){
 function f_tail(f){
 	return function(x){
 		return f(x+1);
-	}
+	};
 }
 
 function f_to_stream(f){
@@ -111,7 +122,7 @@ function int(x){
 
 function sum(f,a,b){
 	var m = 0.5*(a+b);
-	if(b-a<0.00001){
+	if(b-a<0.01){
 		return (b-a)*f(int(m));
 	}else{
 		return sum(f,a,m)+sum(f,m,b);
@@ -132,3 +143,11 @@ function smooth_regenerated_whisper(s,r,i){
 		return smooth_regenerated_whisper(f_to_stream(smooth(to_fun(s),r)),r,i-1);
 	}
 }
+ display(eval_stream(smooth_regenerated_whisper(regenerate_truncated_whisper(list(4, 7, 2, 5, 8)), 1, 1), 10));
+//[4.75, [5, [4, [5, [0, [-43, [-180, [-491, [-1080, [-2075, []]]]]]]]]]]
+
+// display(eval_stream(smooth_regenerated_whisper(regenerate_truncated_whisper(list(4, 7, 2, 5, 8)), 1, 2), 10));
+//[4.8125, [4.6875, [4.5, [3.5, [-9.5, [-66.5, [-223.5, [-560.5, [-1181.5, [-2214.5, []]]]]]]]]]]
+
+// display(eval_stream(smooth_regenerated_whisper(regenerate_truncated_whisper(list(4, 7, 2, 5, 8)), 1.5, 2), 4));
+//[4.777777777777778, [4.666666666666667, [4.666666666666667, [2.333333333333334, []]]]]
